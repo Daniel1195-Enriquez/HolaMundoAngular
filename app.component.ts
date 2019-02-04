f33a679f44a085a155d1b2a6e25b6224566cf294
Daniel Enriquez <victordanen@gmail.com>
2019-02-04 11:14:06 -0600

    first commit

---
+++ app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Hello world in angular6 by Daniel Enríquez, Yomeri Curiel';
+}
