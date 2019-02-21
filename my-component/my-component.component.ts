61ed5830d21300d40bcaebfd01f1c3078b16521a
yomeri <yomericuriel1@gmail.es>
2019-02-21 02:19:26 -0600

    Se agrego un arreglo de bototes( seccion1,seccion2,seccion3) y titulos

---
+++ my-component/my-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eit-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.styl']
+})
+export class MyComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
